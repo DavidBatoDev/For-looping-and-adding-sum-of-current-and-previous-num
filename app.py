18a29ddca6726b98d7b182945380f6cbfabfe630
David E. Bato bato <149306718+DavidBatoDev@users.noreply.github.com>
2024-01-22 14:00:25 +0800

    Finished the program functionality

--- app.py
+++ app.py
@@ -1,2 +1,10 @@
 # create a for loop with 10 iterations
 # each value of the iteration adds the value of the previous iteration value
+print('Printing current and previous number sum in a range(10)')
+previous_number = 0
+for i in range(10):
+    sum = i + previous_number
+    print(f'Current Number is {i} Previous Number {previous_number} Sum: {sum}')
+    # change the previous number after printing the result:
+    previous_number = i
+    
